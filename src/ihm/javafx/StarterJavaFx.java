368fb10ee7b597852ab3bba06a39aea7a1235032
cb <corentinboleis@hotmail.fr>
2017-09-09 10:38:28 +0200

    Initialisation de la branche javaFx

---
+++ src/ihm/javafx/StarterJavaFx.java
@@ -0,0 +1,15 @@
+package ihm.javafx;
+
+/**
+ * 
+ * @author C.B.
+ *
+ */
+public class StarterJavaFx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
